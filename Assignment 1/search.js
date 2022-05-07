1a9e81143df641fd7f67bc1cdc451ae85b9bc8ea
Kim Chung <kchung68@my.bcit.ca>
2022-05-07 16:53:32 -0700

    adjust history following assignment instruction

--- Assignment 1/search.js
+++ Assignment 1/search.js
@@ -1,16 +1,13 @@
-pokemon_history = new Set();
-
 function get_pokemons() {
     keyword = $("#search_keyword").val();
-    select = document.getElementById("filter");
-    filter = select.options[select.selectedIndex].value;
 
-    //console.log("wrong input");
     if (keyword.toUpperCase() == keyword.toLowerCase()) {
         $("#pokemons_display").text("Invalid input. Must be letters only.")
     } else {
         $("#pokemons_display").empty();
         get_pokemon_by_name(keyword);
+        $("#display_history").append(`<div><button class="retrieve_history" key="name" val=${keyword}>name ${keyword}</button>
+        <button class="remove_history_tag">x</button></div>`)
     }
     $("#search_keyword").val("");
 }
@@ -19,18 +16,8 @@ function get_pokemon_by_name(keyword) {
     $.ajax({
         url: `https://pokeapi.co/api/v2/pokemon/${keyword}`,
         type: "GET",
-        success: process
+        success: process_pokemon
     })
-}
-
-function process(pokemon){
-    $("#pokemons_display").append(`<div class="pokemon"><h4>${pokemon.id}</h4>
-    <img src=${pokemon.sprites.other["official-artwork"]["front_default"]}>
-    <a href="profile.html" id=${pokemon.id} class="pokemon_name">${pokemon.name.toUpperCase()}</a>`)
-    
-    $("#display_history").append(`<div class="pokemon"><h4>${pokemon.id}</h4>
-    <img src=${pokemon.sprites.other["official-artwork"]["front_default"]}>
-    <a href="profile.html" id=${pokemon.id} class="pokemon_name">${pokemon.name.toUpperCase()}</a>`)
 }
 
 function display_options() {
@@ -59,6 +46,8 @@ function get_pokemons_habitat() {
         type: "GET",
         success: get_pokemon
     })
+    $("#display_history").append(`<div><button class="retrieve_history" key="habitat" val=${habitat}>habitat ${habitat}</button>
+    <button class="remove_history_tag">x</button></div>`)
 }
 
 
@@ -84,6 +73,8 @@ function get_pokemons_type() {
         type: "GET",
         success: get_pokemon_one_type
     })
+    $("#display_history").append(`<div><button class="retrieve_history" key="type" val=${type}>type ${type}</button>
+    <button class="remove_history_tag">x</button></div>`)
 }
 
 function get_pokemon_one_type(pokemons){
@@ -109,6 +100,35 @@ function save_to_storage() {
     localStorage.setItem("pokemonID", $(this).attr("id"));
 }
 
+function retrieve_history(){
+    key = $(this).attr("key");
+    val = $(this).attr("val");
+    $("#pokemons_display").empty();
+    if(key == "name"){
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/pokemon/${val}`,
+            type: "GET",
+            success: process_pokemon
+        })
+    }else if(key == "habitat"){
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/pokemon-habitat/${val}`,
+            type: "GET",
+            success: get_pokemon
+        })
+    }else if(key == "type"){
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/type/${val}`,
+            type: "GET",
+            success: get_pokemon_one_type
+        })
+    }
+}
+
+function remove_history_tag(){
+    $(this).parent().remove();
+}
+
 function setup() {
     //get_pokemons_habitat();
     $("button").click(get_pokemons);
@@ -116,6 +136,8 @@ function setup() {
     $('#habitat').on('change', get_pokemons_habitat);
     $('#type').on('change', get_pokemons_type);
     $("body").on("click", ".pokemon_name", save_to_storage);
+    $("body").on("click", ".retrieve_history", retrieve_history);   
+    $("body").on("click", ".remove_history_tag", remove_history_tag);
 }
 
 $(document).ready(setup);
